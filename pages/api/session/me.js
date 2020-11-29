010f2ba311faad65a925e8729ff8bbf5e2bf40be
jaideepghosh <er.jaideepghosh@gmail.com>
2020-11-29 17:56:31 +0530

    feat(me):Added api.

---
+++ pages/api/session/me.js
@@ -0,0 +1,39 @@
+import database from "../../../helpers/database";
+
+export default async (req, res) => {
+  if (!req.query || !req.query.token) {
+    res.status(422).json({ message: "Token required." });
+  }
+  try {
+    const response = await new Promise((resolve, reject) => {
+      database.sessions.findOne(
+        {
+          token: req.query.token
+        },
+        (error, doc) => {
+          if (error) {
+            console.error("me:: database.sessions.findOne:: error::", error);
+            return reject(error);
+          }
+          if (!doc || !doc.user_id) {
+            console.error("me:: database.users.findOne:: doc.user_id is null.");
+            return reject(new Error("Corrupted data found."));
+          }
+          database.users.findOne({ _id: doc.user_id }, (error, user) => {
+            if (error) {
+              console.error("me:: database.users.findOne:: error::", error);
+              return reject(error);
+            }
+            return resolve(user);
+          });
+        }
+      );
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    console.error("me:: error::", error);
+    const message =
+      error && error.message ? error.message : "Internal server error.";
+    res.status(500).json({ message });
+  }
+};
