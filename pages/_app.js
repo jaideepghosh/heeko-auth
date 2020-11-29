7d763ec6cbfce2641a899d930f4f2d361ead7ebf
jaideepghosh <er.jaideepghosh@gmail.com>
2020-11-30 01:19:53 +0530

    fix(app):Added fixes for tostify.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
+import Head from "next/head";
 import "../styles/tailwind.css";
 import "../styles/globals.css";
-import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/react-toastify@6.1.0/dist/ReactToastify.css"
+          rel="stylesheet"
+        />
+      </Head>
       <Component {...pageProps} />
       <ToastContainer />
     </>
