d43681a413d61d6c16e8c8673d0ba093c6be7bc3
jaideepghosh <er.jaideepghosh@gmail.com>
2020-11-30 00:24:17 +0530

    feat(login):Replaced svg.

--- pages/login.js
+++ pages/login.js
@@ -68,17 +68,18 @@ const Login = () => {
     >
       <span className="absolute left-0 inset-y-0 flex items-center pl-3">
         <svg
-          className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          aria-hidden="true"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+          stroke="currentColor"
+          strokeWidth="2"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="animate-spin h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
         >
-          <path
-            fillRule="evenodd"
-            d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-            clipRule="evenodd"
-          />
+          <circle cx="12" cy="12" r="10"></circle>
+          <polyline points="12 6 12 12 16 14"></polyline>
         </svg>
       </span>
       Please wait
