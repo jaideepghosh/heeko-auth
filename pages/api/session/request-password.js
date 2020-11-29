eb4aea5298d3b5bec73fb965d3716277f2cb961b
jaideepghosh <er.jaideepghosh@gmail.com>
2020-11-29 23:26:47 +0530

    feat(request-password):Added feat to send password.

--- pages/api/session/request-password.js
+++ pages/api/session/request-password.js
@@ -1,8 +1,35 @@
 import CryptoJS from "crypto-js";
 import dotenv from "dotenv";
+import nodemailer from "nodemailer";
 import database from "../../../helpers/database";
 
 dotenv.config();
+
+const mailTransporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_SMTP_USER_EMAIL,
+    pass: process.env.GMAIL_SMTP_PASSWORD
+  }
+});
+
+const sendMail = (email, password) => {
+  const mailPayload = {
+    from: `${process.env.GMAIL_SMTP_USER_NAME} <${process.env.GMAIL_SMTP_USER_EMAIL}>`,
+    to: email,
+    subject: "Heeko Auth - Password",
+    text: `Your password is: ${password}`
+  };
+  return new Promise((resolve, reject) => {
+    mailTransporter.sendMail(mailPayload, (error, data) => {
+      if (error) {
+        return reject(error);
+      } else {
+        return resolve("Email sent successfully");
+      }
+    });
+  });
+};
 
 export default async (req, res) => {
   try {
@@ -10,26 +37,31 @@ export default async (req, res) => {
       if (!req.body || !req.body.email) {
         return reject(new Error("Required input values are missing."));
       }
-      database.users.findOne({ email: req.body.email }, {}, (error, doc) => {
-        if (error) {
-          console.error(
-            "request-password:: database.users.findOne:: error::",
-            error
-          );
-          return reject(false);
+      database.users.findOne(
+        { email: req.body.email },
+        {},
+        async (error, doc) => {
+          if (error) {
+            console.error(
+              "request-password:: database.users.findOne:: error::",
+              error
+            );
+            return reject(false);
+          }
+          if (!doc) {
+            return reject(new Error("Invalid email address."));
+          }
+          //Check password.
+          const decryptedPassword = CryptoJS.AES.decrypt(
+            doc.password,
+            process.env.ENCRYPTION_SECRET
+          ).toString(CryptoJS.enc.Utf8);
+          await sendMail(doc.email, decryptedPassword);
+          return resolve(true);
         }
-        if (!doc) {
-          return reject(new Error("Invalid email address."));
-        }
-        //Check password.
-        const decryptedPassword = CryptoJS.AES.decrypt(
-          doc.password,
-          process.env.ENCRYPTION_SECRET
-        ).toString(CryptoJS.enc.Utf8);
-        return resolve(decryptedPassword);
-      });
+      );
     });
-    res.status(200).json(user);
+    res.status(200).json({ message: "Please check your mail for password." });
   } catch (error) {
     console.log("request-password:: error::", error);
     const message =
